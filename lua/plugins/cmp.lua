159c7fe8b402f5206b0a3e13d75b93ce364d76a4
edo-tuf <edoardonervo96@gmail.com>
2026-01-10 16:09:36 +0100

    fix cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,35 +1,77 @@
 return {
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter", -- load when entering insert mode
+  event = "InsertEnter",
   dependencies = {
-    "hrsh7th/cmp-nvim-lsp",     -- LSP source
-    "hrsh7th/cmp-buffer",       -- buffer completions
-    "hrsh7th/cmp-path",         -- path completions
-    "hrsh7th/cmp-cmdline",      -- cmdline completions
-    "saadparwaiz1/cmp_luasnip", -- LuaSnip completions
-    "L3MON4D3/LuaSnip",         -- snippet engine
-    "rafamadriz/friendly-snippets", -- common snippets
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
   },
   config = function()
     local cmp = require("cmp")
     local luasnip = require("luasnip")
 
-    require("luasnip.loaders.from_vscode").lazy_load() -- load friendly snippets
-    
-  cmp.setup({
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    local check_backspace = function()
+      local col = vim.fn.col "." - 1
+      return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+    end
+
+    local kind_icons = {
+      Text = "󰊄",
+      Method = "m",
+      Function = "󰊕",
+      Constructor = "",
+      Field = "",
+      Variable = "󰫧",
+      Class = "",
+      Interface = "",
+      Module = "",
+      Property = "",
+      Unit = "",
+      Value = "",
+      Enum = "",
+      Keyword = "󰌆",
+      Snippet = "",
+      Color = "",
+      File = "",
+      Reference = "",
+      Folder = "",
+      EnumMember = "",
+      Constant = "",
+      Struct = "",
+      Event = "",
+      Operator = "",
+      TypeParameter = "󰉺",
+    }
+
+    cmp.setup({
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
       mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-1),
+        ["<C-f>"] = cmp.mapping.scroll_docs(1),
         ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
+          elseif luasnip.expandable() then
+            luasnip.expand()
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
+          elseif check_backspace() then
+            fallback()
           else
             fallback()
           end
@@ -44,29 +86,37 @@ return {
           end
         end, { "i", "s" }),
       }),
-      sources = cmp.config.sources({
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+          vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+          vim_item.menu = ({
+            nvim_lsp = "[LSP]",
+            luasnip = "[Snippet]",
+            buffer = "[Buffer]",
+            path = "[Path]",
+          })[entry.source.name]
+          return vim_item
+        end,
+      },
+      sources = {
         { name = "nvim_lsp" },
         { name = "luasnip" },
-      }, {
         { name = "buffer" },
-      }),
-    })
-
-    -- Cmdline completion
-    cmp.setup.cmdline("/", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" }
-      }
-    })
-
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" }
-      },{
-        { name = "cmdline" }
-      })
+        { name = "path" },
+      },
+      confirm_opts = {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      experimental = {
+        ghost_text = false,
+        native_menu = false,
+      },
     })
   end,
 }
